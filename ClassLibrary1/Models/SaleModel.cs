903e392b48fbbc5a64a7e27d8a8021018a5aafbc
James Walsh <lunajack41@outlook.com>
2020-01-14 19:09:56 -0500

    Add Saving the Cart to the Database

---
+++ ClassLibrary1/Models/SaleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Library.Models
+{
+    public class SaleModel
+    {
+        //this one is used to just send what we need to send to the API
+        public List<SaleDetailModel> SaleDetails { get; set; } = new List<SaleDetailModel>();
+    }
+}
