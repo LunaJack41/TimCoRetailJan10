85df566c9330eb4a491f56ada456dd647d53b896
James Walsh <lunajack41@outlook.com>
2020-01-17 18:35:00 -0500

    Updata Controllers and DataAccess to correrly use Configuration and

--- TRMDataManager.Library/DataAccess/UserData.cs
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,18 @@ namespace TRMDataManager.Library.DataAccess
 {
     public class UserData
     {
+        private readonly IConfiguration _config;
+
+        public UserData(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public List<UserModel> GetUserById(string Id)
         {
-            SqlDataAccess sql = new SqlDataAccess();  //Newing this because we have not implemented Dependency Injection yet
+            SqlDataAccess sql = new SqlDataAccess(_config);
 
-            var p = new { Id = Id };   // this is an anonomous object no type which will require the dynamic statement below
+            var p = new { Id = Id };
 
             var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "TRMData");
 
